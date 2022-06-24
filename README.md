9b61dc562ccbfc5d30188b073cd0bdd52148a3eb
toda <ytoda@d-forest-corp.com>
2022-06-24 15:17:44 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# embed-twitter-web-component
+
+## How to use
+
+```html
+<script defer src="embed-twitter.js"></script>
+<embed-twitter src="https://twitter.com/username.../status/tweetId..." align="left"/>
+```
+
+## Attributes
+
+* src (required) - Tweet URL
+* align - `left` or `center` or `right`
---
+++ src/index.tsx
@@ -0,0 +1,82 @@
+import {h} from "preact";
+import {useCallback, useEffect, useMemo, useRef, useState} from "preact/compat";
+import register from "preact-custom-element";
+import {loadScript} from "./loadScript";
+
+type Props = {
+  src: string
+  align?: 'left' | 'center' | 'right'
+}
+
+const heightStore: {
+  [tweetId: string]: string;
+} = {}
+const containerClassName = 'embed-tweet-container'
+const fallbackLinkClassName = 'embed-tweet-link'
+
+const App = ({src, align = 'center'}: Props) => {
+  const ref = useRef<HTMLDivElement | null>(null);
+  const [frameReady, setFrameReady] = useState(false);
+
+  const tweetId = useMemo(() => {
+    const match = src.match(/https?:\/\/twitter.com\/(.*?)\/status\/(\d+)[/?]?/);
+    if (match && match[2]) {
+      return match[2]
+    }
+    return null
+  }, [src]);
+
+  const attribute = useMemo(() => {
+    return tweetId && heightStore[tweetId]
+      ? `style="min-height: ${encodeURIComponent(heightStore[tweetId])};"`
+      : ''
+  }, [])
+
+  const embedTweet = useCallback(async () => {
+    if (!(src && tweetId)) {
+      console.log(`Invalid tweet URL:${src}`)
+      return
+    }
+
+    if (!(window as any).twttr?.ready) {
+      await loadScript({
+        src: 'https://platform.twitter.com/widgets.js',
+        id: 'twitter-widgets'
+      })
+    }
+
+    const container = ref.current;
+    await (window as any).twttr?.widgets?.createTweet(tweetId, container, {
+      align: align
+    })
+    setFrameReady(true)
+    const iframe = container?.querySelector('iframe')
+    if (!iframe) return
+    setTimeout(() => {
+      heightStore[tweetId] = iframe.style.height
+    }, 1000)
+  }, [])
+
+  useEffect(() => {
+    embedTweet();
+  }, [embedTweet])
+
+  return (
+    <div class={`${containerClassName} ${attribute}`} ref={ref}>
+      {!frameReady && (
+        <div>
+          <a href={src} className={fallbackLinkClassName} rel='nofollow'>{src}</a>
+        </div>
+      )}
+    </div>
+  )
+}
+
+register(
+  App,
+  'embed-twitter',
+  [
+    'src',
+    'align'
+  ]
+)
